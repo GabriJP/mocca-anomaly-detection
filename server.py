34c2e3f075d1fcfbd3c1cbd7c3815be0a8f96338
Gabriel <gabrieljimenez@ugr.es>
2023-04-20 12:01:24 +0200

    First implementation

--- server.py
+++ server.py
@@ -4,7 +4,10 @@ from typing import Callable
 import click
 import flwr as fl
 from flwr.common import Config
+from flwr.server import SimpleClientManager
 from flwr.server.strategy import FedProx
+
+from utils import EarlyStopServer
 
 
 def create_fit_config_fn(epochs: int, warm_up_n_epochs: int, batch_size: int) -> Callable[[int], Config]:
@@ -20,13 +23,26 @@ def create_fit_config_fn(epochs: int, warm_up_n_epochs: int, batch_size: int) ->
 @click.option("--warm_up_n_epochs", type=click.IntRange(1), default=1)
 @click.option("--batch_size", type=click.IntRange(1), default=5)
 @click.option("--proximal_mu", type=click.FloatRange(0, 1), default=1.0)
-def cli(num_rounds: int, epochs: int, warm_up_n_epochs: int, batch_size: int, proximal_mu: float) -> None:
+@click.option("--patience", type=click.IntRange(0), default=0)
+@click.option("--min_delta_pct", type=click.FloatRange(0, 1), default=0.0)
+def cli(
+    num_rounds: int,
+    epochs: int,
+    warm_up_n_epochs: int,
+    batch_size: int,
+    proximal_mu: float,
+    patience: int,
+    min_delta_pct: float,
+) -> None:
     strategy = FedProx(
         on_fit_config_fn=create_fit_config_fn(epochs, warm_up_n_epochs, batch_size), proximal_mu=proximal_mu
     )
     certificates_path = Path.home() / "certs"
     fl.server.start_server(
         server_address="0.0.0.0:8080",
+        server=EarlyStopServer(
+            client_manager=SimpleClientManager(), strategy=strategy, patience=patience, min_delta_pct=min_delta_pct
+        ),
         config=fl.server.ServerConfig(num_rounds=num_rounds),
         strategy=strategy,
         grpc_max_message_length=1024**3,
