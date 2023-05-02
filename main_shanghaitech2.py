33badb94ccae63d5e50451da2f3b76486b460bfd
Gabriel <gabrieljimenez@correo.ugr.es>
2023-05-03 01:03:55 +0200

    Compile bugfix

--- main_shanghaitech2.py
+++ main_shanghaitech2.py
@@ -164,6 +164,7 @@ def main(
     net = ShanghaiTech(data_holder.shape, code_length, load_lstm, hidden_size, num_layers, dropout, bidirectional)
     wandb.watch(net)
     if compile_net:
+        torch.set_float32_matmul_precision("high")
         net = torch.compile(net)
     rc.epochs = 1
     rc.warm_up_n_epochs = 0
