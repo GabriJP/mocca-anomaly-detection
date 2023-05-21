046abbcfda423074981f4eaa9783e4757cfc2b66
Gabriel <gabrieljimenez@correo.ugr.es>
2023-05-21 23:58:21 +0200

    Added git pull to fed execution

--- execute_federated.sh
+++ execute_federated.sh
@@ -6,8 +6,12 @@ git pull
 #eval "$(conda shell.bash hook)"
 #conda activate mocca
 #gid=$(python -c "import wandb ; print(wandb.util.generate_id())")
-gid="really_large_fed_ped"
+gid="not_so_large_fed_ped"
 client_command="./execute.sh --load-lstm --bidirectional --clip-length=16 --code-length=512 --dropout=0.3 --idx-list-enc=3,4,5,6 --wandb_group ${gid}"
+
+ssh xavier "cd mocca-anomaly-detection && git pull"
+ssh platano "cd mocca-anomaly-detection && git pull"
+ssh almogrote "cd mocca-anomaly-detection && git pull"
 
 # shellcheck disable=SC2029
 ssh xavier "nohup ./execute.sh --num_rounds 300 --epochs 2 --warm_up_n_epochs=0 --batch_size 8 --proximal_mu 1 >${gid}.log 2>&1 </dev/null &"
