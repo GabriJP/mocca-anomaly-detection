dd3507d5319dc852d0c9831c9d5ec6677a9ff928
Gabriel <gabrieljimenez@correo.ugr.es>
2023-05-29 17:59:31 +0200

    Heredoc

--- execute_federated.sh
+++ execute_federated.sh
@@ -7,7 +7,9 @@ cd "${HOME}/mocca-anomaly-detection" || exit
 git pull
 
 exec_client() {
-  CMDS=$(cat <<CMD
+  echo "Sending commands to ${CLIENT_NAME}"
+    # shellcheck disable=SC2087
+    ssh "$CLIENT_NAME" bash <<EOC
 cd "\${HOME}/mocca-anomaly-detection" || exit
 rm -r output/* nohup.out 2>/dev/null
 git pull
@@ -16,14 +18,13 @@ eval "\$(conda shell.bash hook)"
 conda activate mocca || exit
 export FLWR_TELEMETRY_ENABLED=0
 python fed.py client $COMMON_OPTS --data-path $DATA_PATH --wandb_name $WANDB_NAME >${GID}.log 2>&1 </dev/null &
-CMD
-  )
-    # shellcheck disable=SC2029
-    ssh "$CLIENT_NAME" "nohup $CMDS >${GID}.log 2>&1 </dev/null &"
+EOC
 }
 
 # shellcheck disable=SC2029
+echo "Starting server"
 python fed.py server --num_rounds 50 --epochs 2 --warm_up_n_epochs=0 --batch_size 8 --proximal_mu 1 >"${GID}.log" 2>&1 </dev/null &
+echo "Delay"
 sleep 10
 
 DATA_PATH="data/UCSDped2"
@@ -36,6 +37,7 @@ WANDB_NAME="almogrote"
 CLIENT_NAME="almogrote"
 exec_client
 
+echo "Waiting for server to finish"
 wait %1
 
 # ffmpeg -http_persistent 0 -protocol_whitelist file,http,https,tcp,tls,crypto -i url.m3u8 -c copy video.mp4
