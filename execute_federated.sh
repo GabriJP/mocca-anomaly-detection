a475acc60d8a8eb0a87c741d0205d882b2eb4401
Gabriel <gabrieljimenez@correo.ugr.es>
2023-06-01 18:17:59 +0200

    Fed ped platano+cititc

--- execute_federated.sh
+++ execute_federated.sh
@@ -25,21 +25,21 @@ EOC
 echo "Starting server"
 eval "$(conda shell.bash hook)"
 conda activate mocca
-nohup python fed.py server --num_rounds 50 --epochs 2 --warm_up_n_epochs=0 --proximal_mu 1 >"${GID}.log" 2>&1 </dev/null &
+nohup python fed.py server --num_rounds 20 --epochs 2 --warm_up_n_epochs=0 --proximal_mu 1 >"${GID}.log" 2>&1 </dev/null &
 #SERVER_PID=$!
 echo "Delay"
 sleep 10
 
-DATA_PATH="data/UCSDped2"
+DATA_PATH="data/UCSDped1"
 WANDB_NAME="platano"
 CLIENT_NAME="platano"
 BATCH_SIZE=8
 exec_client
 
-DATA_PATH="data/UCSDped1"
-WANDB_NAME="almogrote"
-CLIENT_NAME="almogrote"
-BATCH_SIZE=16
+DATA_PATH="data/UCSDped2"
+WANDB_NAME="citic"
+CLIENT_NAME="citic"
+BATCH_SIZE=4
 exec_client
 
 #echo "Waiting for server to finish"
