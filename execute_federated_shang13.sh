bf642b24d8b5828a6bdb39f0f0637bc8b9f9f6b7
Gabriel <gabrieljimenez@correo.ugr.es>
2023-06-08 07:47:27 +0200

    Dataset refactor

--- execute_federated_shang13.sh
+++ execute_federated_shang13.sh
@@ -43,7 +43,7 @@ do
 done
 
 CLIENT_NAME="platano"
-for i in 07 06 02 09
+for i in 07 06 09 11
 do
   DATA_PATH="data/shang$i"
   WANDB_NAME="${CLIENT_NAME}_$i"
@@ -51,7 +51,7 @@ do
 done
 
 CLIENT_NAME="citic"
-for i in 10 11
+for i in 10 02
 do
   DATA_PATH="data/shang$i"
   WANDB_NAME="${CLIENT_NAME}_$i"
