561e730cdd26da124cdb16fdc4a79aaf892ea5b7
Gabriel <gabriel@jimpere.com>
2023-07-03 08:57:06 +0100

    Updated fed script

--- execute_federated_ped.sh
+++ execute_federated_ped.sh
@@ -23,26 +23,24 @@ EOC
 
 # shellcheck disable=SC2029
 echo "Starting server"
-eval "$(conda shell.bash hook)"
-conda activate mocca
+#eval "$(conda shell.bash hook)"
+#conda activate mocca
 nohup python fed.py server --num_rounds 20 --epochs 2 --warm_up_n_epochs=0 --proximal_mu 1 >"${GID}_server.log" 2>&1 </dev/null &
 #SERVER_PID=$!
 echo "Delay"
-sleep 10
+sleep 5
 
 DATA_PATH="data/UCSDped1"
+WANDB_NAME="almogrote"
+CLIENT_NAME="almogrote"
+BATCH_SIZE=16
+exec_client
+
+DATA_PATH="data/UCSDped2"
 WANDB_NAME="platano"
 CLIENT_NAME="platano"
 BATCH_SIZE=8
 exec_client
 
-DATA_PATH="data/UCSDped2"
-WANDB_NAME="citic"
-CLIENT_NAME="citic"
-BATCH_SIZE=4
-exec_client
-
 #echo "Waiting for server to finish"
 #wait $SERVER_PID
-
-# ffmpeg -http_persistent 0 -protocol_whitelist file,http,https,tcp,tls,crypto -i url.m3u8 -c copy video.mp4
