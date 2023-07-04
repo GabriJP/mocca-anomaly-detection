d94cf76da13120ad3128e7e3a58dba4fac62a541
Gabriel <gabriel@jimpere.com>
2023-07-04 07:39:01 +0100

    Compile 50 rounds

--- execute_federated_ped.sh
+++ execute_federated_ped.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 GID=${1:-"fed_exp_noname"}
-COMMON_OPTS="--load-lstm --bidirectional --clip-length=16 --code-length=512 --dropout=0.3 --idx-list-enc=3,4,5,6 --wandb_group ${GID}"
+COMMON_OPTS="--load-lstm --bidirectional --clip-length=16 --code-length=512 --dropout=0.3 --idx-list-enc=3,4,5,6 --compile_net --wandb_group ${GID}"
 
 cd "${HOME}/mocca-anomaly-detection" || exit
 git pull
@@ -21,28 +21,19 @@ nohup python fed.py client $COMMON_OPTS --data-path $DATA_PATH --wandb_name $WAN
 EOC
 }
 
-# shellcheck disable=SC2029
 echo "Starting server"
-eval "$(conda shell.bash hook)"
-conda activate mocca
-nohup python fed.py server --num_rounds 20 --epochs 2 --warm_up_n_epochs=0 --proximal_mu 1 >"${GID}_server.log" 2>&1 </dev/null &
-#SERVER_PID=$!
+nohup python fed.py server --num_rounds 50 --epochs 2 --warm_up_n_epochs=0 --proximal_mu 1 >"${GID}_server.log" 2>&1 </dev/null &
 echo "Delay"
-sleep 10
+sleep 5
 
 DATA_PATH="data/UCSDped1"
+WANDB_NAME="almogrote"
+CLIENT_NAME="almogrote"
+BATCH_SIZE=16
+exec_client
+
+DATA_PATH="data/UCSDped2"
 WANDB_NAME="platano"
 CLIENT_NAME="platano"
 BATCH_SIZE=8
 exec_client
-
-DATA_PATH="data/UCSDped2"
-WANDB_NAME="citic"
-CLIENT_NAME="citic"
-BATCH_SIZE=4
-exec_client
-
-#echo "Waiting for server to finish"
-#wait $SERVER_PID
-
-# ffmpeg -http_persistent 0 -protocol_whitelist file,http,https,tcp,tls,crypto -i url.m3u8 -c copy video.mp4
