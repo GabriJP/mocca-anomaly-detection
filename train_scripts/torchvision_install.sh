155e9cf904f64ca2268fd2c0500b6bd43c7a968f
Gabriel <gabrieljimenez@correo.ugr.es>
2023-09-23 14:29:02 +0200

    Torch script

--- train_scripts/torchvision_install.sh
+++ train_scripts/torchvision_install.sh
@@ -5,9 +5,10 @@ pip3 uninstall -y torch torchvision
 TORCH_RELEASE="2.0.1"
 VISION_RELEASE="0.15.2"
 
-sudo apt install -y clang python3-pip cmake libopenblas-dev libopenmpi-dev
+#sudo apt install -y clang python3-pip cmake libopenblas-dev libopenmpi-dev
 
 # Torch
+cd || exit
 [ ! -d pytorch ] && git clone "https://github.com/pytorch/pytorch.git"
 cd pytorch || exit
 git reset --hard
@@ -24,17 +25,18 @@ export PATH=/usr/lib/ccache:$PATH
 #nano torch/utils/cpp_extension.py -> https://gist.github.com/dusty-nv/ce51796085178e1f38e3c6a1663a93a1#file-pytorch-1-11-jetpack-5-0-patch
 #make triton
 pip3 install -r requirements.txt
-pip3 install scikit-build
-pip3 install ninja
-python3 setup.py bdist_wheel
+pip3 install -U scikit-build
+pip3 install -U ninja
+python3 setup.py bdist_wheel || exit
 pip3 install --user -U "dist/torch-${TORCH_RELEASE}-cp38-cp38-linux_aarch64.whl"
 
 # Torchvision
+cd || exit
 [ ! -d vision ] && git clone "https://github.com/pytorch/vision.git"
 cd vision || exit
 git reset --hard
 git pull
 git checkout "v${VISION_RELEASE}"
 export BUILD_VERSION=${VISION_RELEASE} FORCE_CUDA=1 CUDA_VISIBLE_DEVICES=0 CUDA_HOME='/usr/local/cuda' USE_CUDA=1 USE_CUDNN=1
-python3 setup.py bdist_wheel
+python3 setup.py bdist_wheel || exit
 pip3 install --user -U "dist/torchvision-${TORCH_RELEASE}-cp38-cp38-linux_aarch64.whl"
