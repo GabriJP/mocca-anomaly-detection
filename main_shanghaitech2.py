29711dff6d45d93d394f2d51cd77f7d79dbcb3d4
Gabriel <gabrieljimenez@ugr.es>
2023-09-29 11:14:10 +0200

    Bugfix fp16

--- main_shanghaitech2.py
+++ main_shanghaitech2.py
@@ -178,11 +178,11 @@ def main(
     data_holder = DataManager(
         dataset_name="ShanghaiTech", data_path=data_path, normal_class=-1, seed=seed, clip_length=clip_length
     ).get_data_holder()
-    net = ShanghaiTech(data_holder.shape, code_length, load_lstm, hidden_size, num_layers, dropout, bidirectional)
-    wandb.watch(net)
     torch.set_float32_matmul_precision("medium")
     torch.set_default_dtype(torch.float16)
     torch.set_default_tensor_type(torch.HalfTensor)
+    net = ShanghaiTech(data_holder.shape, code_length, load_lstm, hidden_size, num_layers, dropout, bidirectional)
+    wandb.watch(net)
     if compile_net:
         net = torch.compile(net)  # type: ignore
     rc.epochs = 1
