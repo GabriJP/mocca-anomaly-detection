580f2c8cfb2a5e363a4fa9b5edb1b70b766a7bbf
Gabriel <gabrieljimenez@ugr.es>
2023-12-01 13:06:20 +0100

    Disable all

--- trainers/trainer_shanghaitech.py
+++ trainers/trainer_shanghaitech.py
@@ -77,7 +77,6 @@ def train(
     logger.info("Starting training...")
     warm_up_n_epochs = rc.warm_up_n_epochs
     net.train()
-    scaler = torch.cuda.amp.GradScaler()
 
     best_loss = 1e12
     net_checkpoint = Path()
@@ -127,13 +126,10 @@ def train(
                 for k in keys:
                     d_from_c[k] += torch.mean(dist[k]).item()
 
-            scaler.scale(objective_loss_).backward()
+            objective_loss_.backward()
             # if (idx + 1) % 5 == 0 or (idx + 1 == len(train_loader)):
             if True:
-                scaler.unscale_(optimizer)
-                torch.nn.utils.clip_grad_norm_(net.parameters(), max_norm=1.0)
-                scaler.step(optimizer)
-                scaler.update()
+                optimizer.step()
                 # Zero the network parameter gradients
                 optimizer.zero_grad()
 
