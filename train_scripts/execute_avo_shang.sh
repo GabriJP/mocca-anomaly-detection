2fcf5c4b1b8ad7ece49c5478553b37f3c3fce49c
Gabriel <gabriel@jimpere.com>
2023-12-26 17:09:21 +0000

    Add avo script

---
+++ train_scripts/execute_avo_shang.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+client_command="python main_shanghaitech2.py --load-lstm --bidirectional --clip-length=16 --code-length=512 --dropout=0.3 --epochs=21 --test-chk 5,10,15,20 --idx-list-enc=3,4,5,6 --wandb_group shang_avo --es_initial_patience_epochs 51 --es_patience 51 --compile_net"
+
+for subset in data/shanghaitech/avo/*
+do
+  [ -e "$subset" ] || continue
+  ${client_command} -dp "$subset" --wandb_name "$subset" --batch-size 16 >"${subset}.out" 2>&1 </dev/null
+done
