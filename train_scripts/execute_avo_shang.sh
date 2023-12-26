869b6c6d92cc8f59c284db5ac6274021f99ff0cb
Gabriel <gabriel@jimpere.com>
2023-12-26 17:20:27 +0000

    Disable compile in avo

--- train_scripts/execute_avo_shang.sh
+++ train_scripts/execute_avo_shang.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-client_command="python main_shanghaitech2.py --load-lstm --bidirectional --clip-length=16 --code-length=512 --dropout=0.3 --epochs=21 --test-chk 5,10,15,20 --idx-list-enc=3,4,5,6 --wandb_group shang_avo --es_initial_patience_epochs 51 --es_patience 51 --compile_net"
+client_command="python main_shanghaitech2.py --load-lstm --bidirectional --clip-length=16 --code-length=512 --dropout=0.3 --epochs=21 --test-chk 5,10,15,20 --idx-list-enc=3,4,5,6 --wandb_group shang_avo --es_initial_patience_epochs 51 --es_patience 51"
 
 for subset in data/shanghaitech/avo/*
 do
