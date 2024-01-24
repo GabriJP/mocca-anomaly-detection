3a8ef546837e796f288f186ff6813fef000f2b16
Gabriel <gabrieljimenez@correo.ugr.es>
2024-01-24 13:23:11 +0100

    Avo train then test

--- main_shang_avo.py
+++ main_shang_avo.py
@@ -1,9 +1,9 @@
 import logging
 import time
-from contextlib import ExitStack
 from dataclasses import asdict
 from os import cpu_count
 from pathlib import Path
+from typing import Dict
 from typing import List
 from typing import Optional
 from typing import Set
@@ -16,6 +16,8 @@ import wandb
 from datasets.data_manager import DataManager
 from main_shanghaitech2 import MoccaClient
 from models.shanghaitech_model import ShanghaiTech
+from utils import get_out_dir2 as get_out_dir
+from utils import load_model
 from utils import RunConfig
 from utils import set_seeds
 from utils import wandb_logger
@@ -138,64 +140,58 @@ def main(
         ).get_data_holder()
         for path in sorted(data_path.iterdir())
     }
-    with ExitStack() as stack:
-        pre_runs = {
-            i: wandb.init(
-                project="mocca", entity="gabijp", group=wandb_group, name=f"{wandb_prefix}_{i}", config=asdict(rc)
-            )
-            for i in data_holders
-        }
-        runs = {k: v for k, v in pre_runs.items() if isinstance(v, wandb.sdk.wandb_run.Run)}
+    wandb.init(project="mocca", entity="gabijp", group=wandb_group, name="train_run", config=asdict(rc))
 
-        if len(pre_runs) != len(runs):
-            raise ValueError(f"Some runs are not correctly initialized: {len(pre_runs)} vs. {len(runs)}")
+    one_data_holder = next(iter(data_holders.values()))
 
-        for run in runs.values():
-            stack.push(run)  # type: ignore
+    net = ShanghaiTech(one_data_holder.shape, code_length, load_lstm, hidden_size, num_layers, dropout, bidirectional)
 
-        one_data_holder = next(iter(data_holders.values()))
+    if compile_net:
+        torch.set_float32_matmul_precision("high")
+        net = torch.compile(net)  # type: ignore
+    wandb.watch(net)
+    rc.epochs = 1
+    rc.warm_up_n_epochs = 0
 
-        net = ShanghaiTech(
-            one_data_holder.shape, code_length, load_lstm, hidden_size, num_layers, dropout, bidirectional
+    train_loader, _ = one_data_holder.get_loaders(
+        batch_size=rc.batch_size, shuffle_train=True, pin_memory=True, num_workers=rc.n_workers
+    )
+
+    mc = MoccaClient(net, one_data_holder, rc)
+
+    initial_time = time.perf_counter()
+
+    if len(test_chk_remainder):
+        test_chk_set.union(set(range(1, epochs, test_chk_remainder[0])))
+    i = 0
+    for i in range(epochs):
+        mc.fit()
+
+    out_dir, _ = get_out_dir(rc)
+    checkpoints: Dict[int, Path] = {int(path.name.split("_")[2]): path for path in out_dir.iterdir()}
+    for name, data_holder in data_holders.items():
+        wandb.init(
+            project="mocca",
+            entity="gabijp",
+            group=wandb_group,
+            name=f"shang{name}",
+            config=asdict(rc),
+            reinit=True,
+            job_type="test",
         )
-
-        if compile_net:
-            torch.set_float32_matmul_precision("high")
-            net = torch.compile(net)  # type: ignore
-        wandb.watch(net)
-        rc.epochs = 1
-        rc.warm_up_n_epochs = 0
-
-        train_loader, _ = one_data_holder.get_loaders(
-            batch_size=rc.batch_size, shuffle_train=True, pin_memory=True, num_workers=rc.n_workers
-        )
-
-        mc = MoccaClient(net, one_data_holder, rc)
-
-        initial_time = time.perf_counter()
-
-        if len(test_chk_remainder):
-            test_chk_set.union(set(range(1, epochs, test_chk_remainder[0])))
-        i = 0
-        for i in range(epochs):
-            mc.data_holder = one_data_holder
-            mc.fit()
-            if i not in test_chk_set:
-                continue
-            for j, run in runs.items():
-                wandb.run = run
-                mc.data_holder = data_holders[j]
-                mc.evaluate()
-
-        for j, run in runs.items():
-            wandb.run = run
-            mc.data_holder = data_holders[j]
+        for j in test_chk_set:
+            model = load_model(checkpoints[j])
+            mc.net.load_state_dict(model["net_state_dict"])
+            mc.R = model["R"]
+            if not isinstance(model["config"], RunConfig):
+                raise ValueError
+            mc.rc = model["config"]
+            mc.current_epoch = j
+            mc.data_holder = data_holders[name]
             mc.evaluate()
 
-        logging.getLogger().info(
-            f"Fitted in {i + 1} epochs requiring {time.perf_counter() - initial_time:.02f} seconds"
-        )
-        wandb_logger.save_model(dict(net_state_dict=net.state_dict(), R=mc.R))
+    logging.getLogger().info(f"Fitted in {i + 1} epochs requiring {time.perf_counter() - initial_time:.02f} seconds")
+    wandb_logger.save_model(dict(net_state_dict=net.state_dict(), R=mc.R))
 
 
 if __name__ == "__main__":
